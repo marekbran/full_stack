c10c3a33061cd6bbf10dea6313ad1cc41313f6ac
@hermana4 <adam.hermann@aalto.fi>
2024-06-18 02:50:25 +0300

    1.6-1.11

--- 1/exercise_solutions/unicafe/src/App.jsx
+++ 1/exercise_solutions/unicafe/src/App.jsx
@@ -6,6 +6,37 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistics = (props) => {
+  if (props.good === 0 && props.bad === 0 && props.neutral === 0) {
+    return <p>No feedback given</p>;
+  } else {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <td>Good: {props.good}</td>
+          </tr>
+          <tr>
+            <td>Neutral: {props.neutral}</td>
+          </tr>
+          <tr>
+            <td>Bad: {props.bad}</td>
+          </tr>
+          <tr>
+            <td>All: {props.good + props.neutral + props.bad}</td>
+          </tr>
+          <tr>
+            <td>Average: {(props.good - props.bad) / (props.good + props.neutral + props.bad)}</td>
+          </tr>
+          <tr>
+            <td>Positive: {(props.good / (props.good + props.neutral + props.bad)) * 100} %</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -22,7 +53,8 @@ const App = () => {
 
         <h2>Statistics</h2>
 
-        <p>Good: {good}</p> <p>Neutral: {neutral}</p> <p>Bad: {bad}</p>
+        <Statistics good={good} bad={bad} neutral={neutral} />
+        
     </div>
   )
 }
