a0ae054609a315f0a0974dd0bcaedfc1defb0fbf
marekbran <hermann.adam03@gmail.com>
2024-06-26 04:07:03 +0300

    up to 3.8

--- 3/exercise_solutions/phonebook_backend/index.js
+++ 3/exercise_solutions/phonebook_backend/index.js
@@ -1,6 +1,18 @@
 const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
+const { nextTick } = require('process')
 const app = express()
+const fs = require('fs');
+const path = require('path');
+
+app.use(express.json())
+
+morgan.token('params', (req) => {
+  return JSON.stringify(req.params)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :params'))
 
 let persons = [
     { 
@@ -24,6 +36,8 @@ let persons = [
       "number": "39-23-6423122"
     }
 ]
+
+
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -52,6 +66,60 @@ app.get('/info', (request, response) => {
 
     response.send(`<p>Request received at ${currentTime}, ${x} entries in the phonebook</p>`);
 })
+
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+
+    if (!person) {
+        return response.status(404).json({ error: 'Person not found' })
+    }
+
+    persons = persons.filter(note => note.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/persons/:name/:number', (request, response) => {
+  const body = request.params  
+  const id = Math.random(1000)
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'name missing' 
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  } 
+
+  if (persons.find(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'name must be unique' 
+    })
+  }
+
+  if (persons.find(person => person.number === body.number)) {
+    return response.status(400).json({ 
+      error: 'number must be unique' 
+    })
+  }
+
+  const person = {
+    id: id,
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(person)
+  response.json(person)
+    
+
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
