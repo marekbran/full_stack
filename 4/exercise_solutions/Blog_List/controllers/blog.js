7d5819daf9a9b71b7b1808295f31ec4023762372
marekbran <hermann.adam03@gmail.com>
2024-07-09 16:21:10 +0300

    BIG changes

---
+++ 4/exercise_solutions/Blog_List/controllers/blog.js
@@ -0,0 +1,58 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(Blogs => {
+    response.json(Blogs)
+  })
+})
+
+/*
+blogsRouter.get('/:id', (request, response, next) => {
+  Blog.findById(request.params.id)
+    .then(Blog => {
+      if (Blog) {
+        response.json(Blog)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+*/
+blogsRouter.post('/', (request, response, next) => {
+
+  const blog = new Blog(request.body)
+
+  blog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+/*
+blogsRouter.delete('/:id', (request, response, next) => {
+  Blog.findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (request, response, next) => {
+  const body = request.body
+
+  const Blog = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Blog.findByIdAndUpdate(request.params.id, Blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog)
+    })
+    .catch(error => next(error))
+})
+*/
+module.exports = blogsRouter
--- 4/exercise_solutions/Blog_List/models/blog.js
+++ 4/exercise_solutions/Blog_List/models/blog.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -8,20 +7,13 @@ const blogSchema = new mongoose.Schema({
   likes: Number
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
   })
 
-  module.exports = mongoose.model('Blog', blogSchema)
+
+module.exports = mongoose.model('Blog', blogSchema)
