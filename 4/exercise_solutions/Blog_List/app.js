33ec7e6aeb9251b909f78d6ed526084418c29071
marekbran <hermann.adam03@gmail.com>
2024-07-12 01:17:22 +0300

    Up to 4.16

--- 4/exercise_solutions/Blog_List/app.js
+++ 4/exercise_solutions/Blog_List/app.js
@@ -4,6 +4,8 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blog')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -27,6 +29,9 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
